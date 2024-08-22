c40d8d57c26f0329082baf857d2da541730082fb
AlinaPetcu <150749536+AlinaPetcu@users.noreply.github.com>
2024-08-22 15:35:12 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import css from'./Loader.module.css'; // Create this CSS file
+mport React, { Component } from 'react';
 
-const Loader = () => {
+import { Audio } from 'react-loader-spinner';
+
+export default class CustomLoader extends Component {
+  render() {
     return (
-        <div  className={css.loader}></div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="three-dots-loading"
+          wrapperStyle
+          wrapperClass
+        />
+        ;
+      </div>
     );
-};
-
-export default Loader;
+  }
+}
